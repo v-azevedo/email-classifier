4b7111feafd120662ebceca73d712a8b9f6a7ba0
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-08-27 15:26:00 -0300

    feat: add rate limiter

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI, UploadFile, HTTPException
+from fastapi import FastAPI, UploadFile, HTTPException, status, Request
+from datetime import datetime, timedelta
 from fastapi.responses import JSONResponse
+from collections import defaultdict
 from pydantic import BaseModel
 from google import genai
+from typing import Dict
 
 from uuid import uuid4
 import pypdf
@@ -20,6 +23,39 @@ logger = logging.getLogger(__name__)
 
 class FileUpload(BaseModel):
     base64_string: str
+
+# Rate limiter storage and settings
+rate_limit_data: Dict[str, list] = defaultdict(list)
+RATE_LIMIT = 20
+TIME_WINDOW = timedelta(minutes=1)
+
+# Rate limiter function
+def rate_limiter(request: Request):
+    if(request.client == None): 
+       raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Client could not be identified.")
+
+    client_ip = request.client.host
+    now = datetime.now()
+
+    # Cleanup old requests beyond the time window
+    rate_limit_data[client_ip] = [timestamp for timestamp in rate_limit_data[client_ip] if now - timestamp < TIME_WINDOW]
+
+    # Enforce rate limiting
+    if len(rate_limit_data[client_ip]) >= RATE_LIMIT:
+        raise HTTPException(status_code=status.HTTP_429_TOO_MANY_REQUESTS, detail="Rate limit exceeded.")
+
+    # Log the current request
+    rate_limit_data[client_ip].append(now)
+
+
+@app.middleware('http')
+async def add_rate_limit(request: Request, call_next):
+    try:
+        rate_limiter(request)
+        response = await call_next(request)
+    except HTTPException as e:
+        response = JSONResponse(status_code=e.status_code, content={"detail": e.detail})
+    return response
 
 @app.post('/upload')
 async def upload_file(file: UploadFile):
@@ -59,7 +95,7 @@ async def upload_info(file_id: str):
         else:
             classification = "Unproductive"
 
-        reply = response.text[len(classification):len(response.text) - 1]
+        reply = response.text[len(classification):len(response.text)]
 
         return JSONResponse(content={'classification': classification, 'reply': reply}, status_code=200)        
     except Exception as e:
