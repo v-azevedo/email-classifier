5e8c0c756f22f82ef7f001d1a03725bfbb5b29fe
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-08-27 16:16:02 -0300

    fix: file creation could be broken if txt file type

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from collections import defaultdict
 from pydantic import BaseModel
 from google import genai
 from typing import Dict
-
 from uuid import uuid4
 import pypdf
 import logging
@@ -17,7 +16,7 @@ client = genai.Client(api_key=os.getenv('GEMINI_API_KEY'))
 UPLOAD_FOLDER = 'uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-file_store = {}
+file_store : Dict[str, str] = {}
 
 logger = logging.getLogger(__name__)
 
@@ -47,7 +46,7 @@ def rate_limiter(request: Request):
     # Log the current request
     rate_limit_data[client_ip].append(now)
 
-
+# Include middleware to enforce rate limiter
 @app.middleware('http')
 async def add_rate_limit(request: Request, call_next):
     try:
@@ -57,13 +56,18 @@ async def add_rate_limit(request: Request, call_next):
         response = JSONResponse(status_code=e.status_code, content={"detail": e.detail})
     return response
 
+# Route responsible for the upload of pdf/txt files
 @app.post('/upload')
 async def upload_file(file: UploadFile):
     try:
         file_id = str(uuid4())  # Generate unique file ID
-        
-        file_path = os.path.join(UPLOAD_FOLDER, f"{file_id}-converted_pdf.pdf")
-        
+        file_path = ""
+
+        if(file.content_type == 'application/pdf'):
+            file_path = os.path.join(UPLOAD_FOLDER, f"{file_id}.pdf")
+        else:
+            file_path = os.path.join(UPLOAD_FOLDER, f"{file_id}.txt")
+
         with open(file_path, 'wb') as output_file:
             output_file.write(await file.read())
 
@@ -73,18 +77,28 @@ async def upload_file(file: UploadFile):
         # Log and return error in case of failure
         logger.error(f"File upload failed: {str(e)}")
         return JSONResponse(content={"error": str(e)}, status_code=500)
-    
+
+# Route responsible for extracting the text from the pdf/txt file and call the LLM api to request a classification and reply
 @app.get('/classify')
 async def upload_info(file_id: str):
     try:
         if file_id not in file_store:
             raise HTTPException(status_code=404, detail="File not found")
 
-        file_path = file_store[file_id]
-        reader = pypdf.PdfReader(file_path)
+        file_path = file_store[file_id];
+        extracted_email = ""
 
+        # Checks if the file stored is a pdf or txt file
+        if(file_path.find('.pdf') != -1):
+            reader = pypdf.PdfReader(file_path)
+            extracted_email = reader.pages[0].extract_text()
+        else:
+            with open(file_path, 'rt') as output_file:
+                extracted_email = output_file.read()
+
+        # Calls the LLM api to request a generative text content based on the input from the extracted text
         response = client.models.generate_content(
-             model="gemini-2.5-flash", contents="Only return the classification for the following email text as Productive or Unproductive(create an appropriate response for the sender with no line breaks): " + reader.pages[0].extract_text()
+             model="gemini-2.5-flash", contents="Only return the classification for the following email text as Productive or Unproductive(create an appropriate response for the sender with no line breaks): " + extracted_email
         )
 
         if(response.text == None):
