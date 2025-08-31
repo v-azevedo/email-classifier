f283acf92791dabc4d174d53c3fc0783fee7fe03
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-08-31 15:01:38 -0300

    fix: allow fronted hosted to access

--- classifier-api/main.py
+++ classifier-api/main.py
@@ -14,8 +14,7 @@ app = FastAPI()
 client = genai.Client(api_key=os.getenv('GEMINI_API_KEY'))
 
 origins = [
-    "https://email-classifier-t597.onrender.com",
-    "http://email-classifier-t597.onrender.com",
+    "https://classifyai.onrender.com/",
     "http://localhost",
     "http://localhost:5000"
 ]
