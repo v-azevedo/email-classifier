495ddc7c4df9aa8c5c18cec43c00a21fd0d30185
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-08-31 13:00:58 -0300

    feat: create text area input component

---
+++ classifier-ui/src/components/TextAreaInput.tsx
@@ -0,0 +1,19 @@
+type Props = {
+  onChange: (text: string) => void;
+  value: string;
+};
+
+const TextAreaInput = ({ onChange, value }: Props) => {
+  return (
+    <div className="flex-3 bg-white border border-gray-200 rounded-3xl p-4">
+      <textarea
+        className="w-full h-full font-quicksand-400"
+        placeholder="Input your text here..."
+        value={value}
+        onChange={(input) => onChange(input.currentTarget.value)}
+      />
+    </div>
+  );
+};
+
+export default TextAreaInput;
